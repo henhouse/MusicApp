55bb7113675d2901a772098eccfb8ffb9284e7b3
Henry Henderson <henry925@gmail.com>
2015-04-24 18:07:21 -0400

    Tabs -> Spaces to fix Git committing intention.

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
 (function () {
-	"use strict";
-	
-	var express = require('express3'),
-			http = require('http'),
-			mongoose = require('mongoose'),
-			app = express();
+    "use strict";
+    
+    var express = require('express3'),
+            http = require('http'),
+            mongoose = require('mongoose'),
+            app = express();
 
-	app.use(express.static(__dirname));
-	app.use(express.urlencoded());
-	
-	mongoose.connect('mongodb://localhost/music');
-	
-	var MusicianSchema = mongoose.Schema({
-		"name": String,
-		"genre": String,
+    app.use(express.static(__dirname));
+    app.use(express.urlencoded());
+    
+    mongoose.connect('mongodb://localhost/music');
+    
+    var MusicianSchema = mongoose.Schema({
+        "name": String,
+        "genre": String,
         "albums" : String, // these two will need to be arrays???
-		"songs": String
-	});
-	var Musician = mongoose.model('Musician', MusicianSchema);
+        "songs": String
+    });
+    var Musician = mongoose.model('Musician', MusicianSchema);
 
-	http.createServer(app).listen(3000);
-	
-	app.get("/getMusician", function(req, res) {
-		Musician.find(req.query, function(err, musician) {
-			if (err) {
-				console.log(err);
-			} else {
-				res.json(musician);
-			}
-		});
-	});
-	
-	app.post("/putMusician", function(req, res) {
-		var newMusician = new Musician(req.body);
-		newMusician.save(function(error, data) {
-			if (error) console.log(error);
-		});
-	});
-	
-	app.post("/removeMusician", function(req, res) {
-		var oldMusician = new Musician(req.body);
-		oldMusician.remove(function(error, data) {
-			if (error) console.log(error);
-		});
-	});
+    http.createServer(app).listen(3000);
+    
+    app.get("/getMusician", function(req, res) {
+        Musician.find(req.query, function(err, musician) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.json(musician);
+            }
+        });
+    });
+    
+    app.post("/putMusician", function(req, res) {
+        var newMusician = new Musician(req.body);
+        newMusician.save(function(error, data) {
+            if (error) console.log(error);
+        });
+    });
+    
+    app.post("/removeMusician", function(req, res) {
+        var oldMusician = new Musician(req.body);
+        oldMusician.remove(function(error, data) {
+            if (error) console.log(error);
+        });
+    });
 
-	console.log("Server listening on port 3000."); 
+    console.log("Server listening on port 3000."); 
 
 }());
--- music.js
+++ music.js
@@ -1,31 +1,31 @@
 (function() {
-	var app = angular.module('music', []);
+    var app = angular.module('music', []);
 
-	app.controller('MusicianController', ['$scope', function($scope) {
-		$scope.name = '';
+    app.controller('MusicianController', ['$scope', function($scope) {
+        $scope.name = '';
         $scope.genre = '';
         $scope.albums = '';
         $scope.songs = '';
 
-		$.getJSON('getMusician', function(result) {
-			$scope.musicians = result;
-		});
+        $.getJSON('getMusician', function(result) {
+            $scope.musicians = result;
+        });
 
-		$scope.add = function() {
-			var newMusician = {
-				"name" : $scope.name,
-				"genre" : $scope.genre,
-				"albums" : $scope.albums,
+        $scope.add = function() {
+            var newMusician = {
+                "name" : $scope.name,
+                "genre" : $scope.genre,
+                "albums" : $scope.albums,
                 "songs" : $scope.songs
-			};
-			$scope.musicians.push(newMusician);
-			$.post('putMusician', newMusician);
-		  $scope.name = $scope.genre = $scope.albums = $scope.songs = '';
-		};
-		
-		$scope.remove = function(musician) {
-			$scope.musicians.splice($scope.musicians.indexOf(musician), 1);
-			$.post('removeMusician', musician);
-		};									 
-	}]);
+            };
+            $scope.musicians.push(newMusician);
+            $.post('putMusician', newMusician);
+          $scope.name = $scope.genre = $scope.albums = $scope.songs = '';
+        };
+        
+        $scope.remove = function(musician) {
+            $scope.musicians.splice($scope.musicians.indexOf(musician), 1);
+            $.post('removeMusician', musician);
+        };                                     
+    }]);
 }());
