f95e77c07955503c8261073bc4ee3d03d9292583
Andrew Madden <acmaddencs@gmail.com>
2015-04-24 12:21:28 -0400

    Updated format to mimmick Shaeffers.

--- insertion.js
+++ insertion.js
@@ -1,18 +1,30 @@
-$(document).ready(function(){
-    $('button').click(function(e){
-        e.preventDefault(); //This is to stop the page from reloading when clicking the ADD
-        //make a row clone
-        var $newRow = $('.row:first-child').clone();//clones class="row",id="artistContainer"
-        //copy variables from text fields
-        var $artist = $('#inputArtist').val();
-        var $genre = $('#inputGenre').val();
-        var $songs = $('#inputSongs').val();
-        var $album = $('#inputAlbums').val();
-        //append variables to related elements in the cloned row
-        
-        //append to content
-        $('#content').append($newRow);
-                             
-    });
-});
 
+(function () {
+	"use strict";
+	
+	var main = function () {
+  
+        $('button').click(function(e){
+            e.preventDefault(); //This is to stop the page from reloading when clicking the ADD
+
+            //clones a whole row
+            var $copy = $('#artistContainer').clone(true);
+            
+            //copy variables from text fields
+            var $artist = $('#inputArtist').val();
+            var $genre = $('#inputGenre').val();
+            var $songs = $('#inputSongs').val();
+            var $album = $('#inputAlbums').val();
+
+            //change variables to related elements in the cloned row
+           
+            //append to content
+            $('#content').append($copy);  
+
+        });
+	};
+	
+	$(document).ready(function () {
+		main();
+	});
+}());
