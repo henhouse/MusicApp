34ba1ad35077c1f955b9efb3256b20a39bafbddc
Andrew Madden <acmaddencs@gmail.com>
2015-04-24 08:41:38 -0400

    Small changes.

--- insertion.js
+++ insertion.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
-    $("p").click(function(){
-        $('#content').append($('.row:first-child').clone());
+    $('button').click(function(e){
+        e.preventDefault(); //This is to stop the page from reloading when clicking the ADD
+        //make a row clone
+        var $newRow = $('.row:first-child').clone();//clones class="row",id="artistContainer"
+        //copy variables from text fields
+        var $artist = $('#inputArtist').val();
+        var $genre = $('#inputGenre').val();
+        var $songs = $('#inputSongs').val();
+        var $album = $('#inputAlbums').val();
+        //append variables to related elements in the cloned row
+        
+        //append to content
+        $('#content').append($newRow);
+                             
     });
 });
 
